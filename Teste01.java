a51de2c79dcc93881908fb3bdd767888f472d459
LUCAS SHIMAZAKI BATISTTI <lucas.batistti@alunos.sc.senac.br>
2025-03-28 15:15:20 -0300

    java

---
+++ Teste01.java
@@ -0,0 +1,5 @@
+public class Teste01{
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ Teste02.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Teste02 {
+    
+    public static void main(String[] args) {
+    System.out.print("Qual seria o seu primeiro numero?");
+    int X = new Scanner(System.in).nextInt();
+    System.out.print("Qual o seu segundo numero?");
+    int Y = new Scanner(System.in).nextInt();
+    int Soma = (X + Y);
+    System.out.print("A soma dos seus dois numeros sao: " + Soma);8
+}
+}
