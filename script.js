f896582cd6c48b2aa96f02680f02117e2641529f
LUCAS SHIMAZAKI BATISTTI <lucas.batistti@alunos.sc.senac.br>
2025-03-28 16:18:17 -0300

    promisse

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
-function loadLogradouro(cep){
-    fetch(`http://viacep.com.br/ws/${cep}/json`)
-    .then(data => data.json())
-    .then(res => {
-        console.log(res)
-    console.log(res.logradouro)
-})    
-}
-loadLogradouro('89062256')
+let promise = new Promise((res,err)=>{
+    let x = 0
+    if(x == 0){
+      res('Ok tudo certo')
+    }
+    else{
+      err('alles kaput')
+    }
+ }) 
+
+ function executar(){
+    promise.then(
+      data => showMessage(data),
+      erro => showMessage(erro))
+    //promisse.catch(erro => showMessage(erro))
+ }
+
+ function showMessage(message){
+    document.getElementById('resultado').innerHTML = message
+ }
